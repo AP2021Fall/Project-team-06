cfe0916b1f9a9d49da54f56250b89a984e14c065
Arvin Ghavidel <arvinghavidel@gmail.com>
2022-01-13 18:06:54 +0330

    Fixed Board and Task models (Team was refactored as needed). Unit tests also added

--- src/test/java/controller/TasksControllerTest.java
+++ src/test/java/controller/TasksControllerTest.java
@@ -2,7 +2,6 @@ package controller;
 
 import model.*;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
